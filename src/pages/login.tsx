d30fd051c5dd29785649de1c76f92580a9ed1b79
subicura <subicura@subicura.com>
2023-02-11 09:37:07 +0900

    환경변수 설정 안내 메시지 추가

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,5 +1,6 @@
-import LoginForm from "@/components/page/login/login-form";
 import GradientBg from "@/components/page/login/gradient-bg";
+import LoginForm from "@/components/page/login/login-form";
+import { Alert } from "antd";
 import { Verified } from "lucide-react";
 
 const LoginPage = () => {
@@ -17,6 +18,26 @@ const LoginPage = () => {
       <div className="w-full lg:w-1/2">
         <div className="relative flex items-center justify-center h-full">
           <section className="w-full px-5 pb-10 text-gray-800 sm:w-4/6 md:w-3/6 lg:w-4/6 xl:w-3/6 sm:px-0">
+            {!process.env.NEXT_PUBLIC_API_ENDPOINT ? (
+              <Alert
+                message="환경변수 설정 오류"
+                description={
+                  <span>
+                    .env.example 파일을 복사하여 .env 파일을 생성해주세요.{" "}
+                    <a
+                      href="https://github.com/purpleio/purple-admin-ui#%EA%B8%B0%EB%B3%B8-%EC%84%A4%EC%A0%95"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      참고 링크
+                    </a>
+                  </span>
+                }
+                type="error"
+                showIcon
+                className="my-10"
+              />
+            ) : null}
             <div className="flex flex-col items-center justify-center px-2 mt-8 sm:mt-0">
               <h2 className="mt-2 text-5xl font-bold leading-tight inter">AWESOME</h2>
               <div className="mt-1 text-lg text-gray-400">Admin System</div>
